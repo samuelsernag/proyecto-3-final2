244110969f586170ce3baa5d3d6f658ceb799b34
isaagui2018-netizen <isaagui2018@gmail.com>
2025-11-25 11:28:52 -0500

    Refactor Usuario class constructor and methods

--- clase_usuario.py
+++ clase_usuario.py
@@ -1,10 +1,33 @@
 class Usuario:
-    def __init__(self, id_usuario, nombre, correo, contrasena):
-        self.id_usuario = id_usuario
+    def __init__(
+        self, 
+        id: int, 
+        nombre: str, 
+        edad: int, 
+        sexo: str,
+        documento: int,
+        correo: str,
+        contrasena: str,
+        tipo: str
+    ):
+        self.id = id
         self.nombre = nombre
+        self.edad = edad
+        self.sexo = sexo
+        self.documento = documento
         self.correo = correo
         self.contrasena = contrasena
+        self.tipo = tipo
+        self.notificaciones = []  # List<Notificacion>
 
-    def actualizar_datos(self, nombre, correo):
-        self.nombre = nombre
-        self.correo = correo
+    def mostrarPerfil(self):
+        pass
+
+    def iniciarSesion(self, correo, contrasena) -> bool:
+        pass
+
+    def cerrarSesion(self):
+        pass
+
+    def actualizarDatos(self, nombre, correo):
+        pass
