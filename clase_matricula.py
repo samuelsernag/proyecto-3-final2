876e553512709acfaf4b6be892dd28038a168c79
isaagui2018-netizen <isaagui2018@gmail.com>
2025-11-25 11:37:00 -0500

    Add Matricula class and remove GestorAcademico methods

---
+++ clase_matricula.py
@@ -0,0 +1,8 @@
+class Matricula:
+    def __init__(self, idMatricula: int, fecha, estado: str):
+        self.idMatricula = idMatricula
+        self.fecha = fecha
+        self.estado = estado
+
+    def calcularCosto(self) -> float:
+        pass
