0de4d7c8784caed8263d0f30bab6c26454331203
isaagui2018-netizen <isaagui2018@gmail.com>
2025-11-25 11:28:16 -0500

    Refactor CalendarioAcademico to include year and semester

--- clase_calendario_academico.py
+++ clase_calendario_academico.py
@@ -1,10 +1,15 @@
 class CalendarioAcademico:
-    def __init__(self):
-        self.actividades = []
-        self.fechas_importantes = []
+    def __init__(self, anio: int, semestre: int):
+        self.anio = anio
+        self.semestre = semestre
+        self.fechasImportantes = []  # List<String>
+        self.eventos = []            # List<String>
 
-    def agregar_actividad(self, actividad):
-        self.actividades.append(actividad)
+    def agregarEvento(self, evento: str):
+        self.eventos.append(evento)
 
-    def agregar_fecha_importante(self, fecha):
-        self.fechas_importantes.append(fecha)
+    def agregarFechaImportante(self, fecha: str):
+        self.fechasImportantes.append(fecha)
+
+    def mostrarCalendario(self):
+        pass
